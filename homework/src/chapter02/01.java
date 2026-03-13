897d35620a3535dcc2bd60970ed9053f3a52de8b
“Lvyizhuo” <lvonezhuo@163.com>
2026-03-13 20:25:39 +0800

    123

---
+++ homework/src/chapter02/01.java
@@ -0,0 +1,13 @@
+public  class var01 {
+    public static void main(String[] args) {
+        //
+        char c1 = '\n';
+        char c2 = '\r';
+        char c3 = '\t';
+        char c4 = '\b';
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
---
+++ homework/src/chapter02/02.java
@@ -0,0 +1,18 @@
+public class 02 {
+    public static void main(String[] args) {
+
+        String str1 = "天龙八部";
+        String str2 = "笑傲江湖";
+        System.out.println(str1 + str2);
+
+        char gender1 =  '男';
+        char gender2 = '女';
+        System.out.println(gender1 + gender2);
+
+        float price1 = '123.123';
+        float price2 = '321.321';
+        System.out.println(price1 + price2);
+    }
+
+
+}
