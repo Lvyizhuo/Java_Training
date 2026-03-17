ef22d691844171e201b1d068e99585f0dca56532
“Lvyizhuo” <lvonezhuo@163.com>
2026-03-17 14:31:24 +0800

    break练习

--- demo/src/Circle/Cricle02.java
+++ demo/src/Circle/Cricle02.java
@@ -1,15 +1,17 @@
 public class Cricle02{
 	public static void main(String[] args) {
-		int i = 1;
+		// int i = 1;
+		int n = 0;
 		int sum = 0;
-		for ( i=1 ; i <= 100 ; i++ ) {
+		for ( int i=1 ; i <= 100 ; i++ ) {
 			sum += i;
 			if (sum > 20) {
 				System.out.println("和>20,和为："+ sum + '\n');
-				System.out.println("当前数为"+ i);
+				// System.out.println("当前数为"+ i);
+				n = i;
 				break;
 			}
-			
 		}
+		System.out.println("当前数为："+ n);
 	}
 }
