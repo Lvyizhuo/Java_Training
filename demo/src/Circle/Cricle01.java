7bbbc1bdcd0b8e4a88cf40e77e9d4a49057cfdea
“Lvyizhuo” <lvonezhuo@163.com>
2026-03-17 09:38:36 +0800

    for循环/while循环练习

---
+++ demo/src/Circle/Cricle01.java
@@ -0,0 +1,22 @@
+public class Cricle01{
+	public static void main(String[] args) {
+		// int i = 1;
+		// int n = 200;
+		// while (i < n){
+		// 	if (i % 3 == 0){
+		// 		System.out.println(i);
+		// 	}
+		// 	i++;
+		// }
+
+		// 2.输出40-200之间的偶数
+		int j = 40;
+		while (j < 200){
+			if (j % 2 == 0) {
+				System.out.println(j);
+				
+			}
+			j++;
+		}
+	}
+}
